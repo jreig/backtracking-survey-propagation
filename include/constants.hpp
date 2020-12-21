e72a177d2ed443f1f6688222f11ca0b150daedf5
Joan Reig <joan.reig27@gmail.com>
2020-12-21 16:52:45 +0100

    Refactorize command line arguments

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,21 +1,40 @@
 #pragma once
 
-namespace sat {
+#include <string>
 
-// SID parameters
-#define DEFAULT_DECIMATION_FRACTION 0.0
+namespace sat::constants {
+  // SID parameters
+  const bool Default_SID_BacktrackingEnabled = false;
+  const double Default_SID_DecimationFraction = 0.0;
 
-// Survey Propagation parameters
-#define DEFAULT_SP_MAX_ITERATIONS 1000
-#define DEFAULT_SP_EPSILON 0.001
+  // SP parameters
+  const unsigned Default_SP_MaxIterations = 1000;
+  const double Default_SP_Epsilon = 0.001;
 
-// WALKSAT parameters
-#define DEFAULT_WS_MAX_TRIES 100
-#define DEFAULT_WS_MAX_FLIPS_COEFICIENT 100
-#define DEFAULT_WS_NOISE 0.57
+  // WALKSAT parameters
+  const double Default_WS_Noise = 0.57;
+  const unsigned Default_WS_MaxTries = 100;
+  const unsigned Default_WS_MaxFlipsCoeficient = 100;
 
-// Solver parameters
-#define DEFAULT_SEED 0
-#define DEFAULT_METRICS_FILE "metrics.csv"
-#define SOLUTION_FILE_EXTENSION ".sol"
-}
+  // Solver parameters
+  const unsigned long Default_Seed = 0;
+  const std::string MetricsFileExtension = ".csv";
+  const std::string SolutionFileExtension = ".sol";
+
+  // Command line arguments help strings
+  const std::string CNF_FileHelp = "File in DIMACS format of the CNF to be solved.";
+
+  const std::string SID_BacktrackingEnabledHelp = "Flag to enable the use of backtracking in the SID algorithm.";
+  const std::string SID_DecimationFractionHelp =
+      "Fraction of unsaigned variables that will be assigned in the decimation process of the SID algorithm.";
+
+  const std::string SP_MaxIterationsHelp = "SP maximum number of iterations.";
+  const std::string SP_EpsilonHelp = "SP convergence threshold.";
+
+  const std::string WS_NoiseHelp = "WALKSAT noise value";
+  const std::string WS_MaxTriesHelp = "WALKSAT maximum tries.";
+  const std::string WS_MaxFlipsCoeficientHelp =
+      "Coeficient used to calculate WALKSAT maximum flips by multiplying it by the number of variables in the CNF.";
+
+  const std::string SeedHelp = "Seed value for reproducibility.";
+}  // namespace sat::constants
--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,56 @@
-#include <string>
 #include <iostream>
+#include <string>
 
 // Project includes
-#include <constants.hpp>
 #include <cmdparser/cmdparser.hpp>
+#include <constants.hpp>
 
 using namespace std;
-using namespace sat;
+using namespace sat::constants;
 
+void configure_parser(cli::Parser &parser) {
+  parser.set_required<string>("f", "cnf-file", CNF_FileHelp);
 
-void configure_parser(cli::Parser& parser) {
-	parser.set_required<string>("c", "cnf-file", "CNF file to solve in DIMACS format.");
+  parser.set_optional<bool>("b", "backtracking", Default_SID_BacktrackingEnabled, SID_BacktrackingEnabledHelp);
+  parser.set_optional<double>("d", "decimation-fraction", Default_SID_DecimationFraction, SID_DecimationFractionHelp);
 
-  parser.set_optional<bool>("b", "backtracking", false, "Uses the backtracking method to solve the CNF.");
-  parser.set_optional<string>("m", "metrics-output", DEFAULT_METRICS_FILE, "Metrics output file in csv format. If the file already exists, the metrics are appended.");
-  parser.set_optional<unsigned long>("s", "seed", DEFAULT_SEED, "Seed for reproducibility.");
+  parser.set_optional<unsigned>("i", "sp-iterations", Default_SP_MaxIterations, SP_MaxIterationsHelp);
+  parser.set_optional<double>("e", "sp-epsilon", Default_SP_Epsilon, SP_EpsilonHelp);
 
-  parser.set_optional<double>("f", "sid-fraction", DEFAULT_DECIMATION_FRACTION, "Fraction of unsaigned variables that will be assigned in the decimation process of the SID algorithm. Value of 0 means one variable at a time.");
+  parser.set_optional<double>("n", "ws-noise", Default_WS_Noise, WS_NoiseHelp);
+  parser.set_optional<unsigned>("t", "ws-tries", Default_WS_MaxTries, WS_MaxTriesHelp);
+  parser.set_optional<unsigned>("c", "ws-flips-coeficient", Default_WS_MaxFlipsCoeficient, WS_MaxFlipsCoeficientHelp);
 
-	parser.set_optional<unsigned>("i", "sp-max-iterations", DEFAULT_SP_MAX_ITERATIONS, "Survey Propagation maximum number of iterations.");
-  parser.set_optional<double>("e", "sp-epsilon", DEFAULT_SP_EPSILON, "Survey Propagation convergence threshold.");
-
-	parser.set_optional<unsigned>("t", "ws-max-tries", DEFAULT_WS_MAX_TRIES, "WALKSAT maximum tries.");
-  parser.set_optional<unsigned>("j", "ws-max-flips-c", DEFAULT_WS_MAX_FLIPS_COEFICIENT, "WALKSAT maximum flips coeficient. The number provided will be multiplied by the CNF number of variables to obtain maximum flips.");
-	parser.set_optional<double>("n", "ws-noise", DEFAULT_WS_NOISE, "WALKSAT noise.");
+  parser.set_optional<unsigned long>("s", "seed", Default_Seed, SeedHelp);
 }
 
+int main(int argc, char *argv[]) {
+  // parser arguments
+  cli::Parser parser(argc, argv);
+  configure_parser(parser);
+  parser.run_and_exit_if_error();
 
-int main(int argc, char* argv[]) {
+  string cnf_file = parser.get<string>("f");
 
-// parser arguments
-cli::Parser parser(argc, argv);
-configure_parser(parser);
-parser.run_and_exit_if_error();
+  bool use_backtracking = parser.get<bool>("b");
+  double sid_fraction = parser.get<double>("d");
 
-string cnf_file = parser.get<string>("c");
+  unsigned sp_max_iterations = parser.get<unsigned>("i");
+  double sp_epsilon = parser.get<double>("e");
 
-bool use_backtracking = parser.get<bool>("b");
-string metrics_file = parser.get<string>("m");
-unsigned long seed = parser.get<unsigned long>("s");
+  double ws_noise = parser.get<double>("n");
+  unsigned ws_max_tries = parser.get<unsigned>("t");
+  unsigned ws_max_flips_coeficient = parser.get<unsigned>("c");
 
-double sid_fraction = parser.get<double>("f");
+  unsigned long seed = parser.get<unsigned long>("s");
 
-unsigned sp_max_iterations = parser.get<unsigned>("i");
-double sp_epsilon = parser.get<double>("e");
+  // configure application
 
-unsigned ws_max_tries = parser.get<unsigned>("t");
-unsigned ws_max_flips = parser.get<unsigned>("j");
-double ws_noise = parser.get<double>("n");
+  // read dimcs file and create factor graph
 
-// configure application
+  // run solver
 
+  // process results
 
-// read dimcs file and create factor graph
-
-// run solver
-
-// process results
-
-return 0;
+  return 0;
 }
